245baaf24454a4fc57e8f3dd6701117c49475d08
raniyaptla <ra.niyapoonthala@gmail.com>
2024-03-22 19:46:09 +0530

    Task3:Initialized a react/frontend application

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import { Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <div>
+      <h2>E-AUCTIONING PLATFORM</h2>
+      <Button variant="primary">Primary Button</Button>
+    </div>
+  );
+}
+
+export default App;
