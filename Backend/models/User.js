a2c321c4b7f777721119efe8b6c09e7e4b421447
raniyaptla <ra.niyapoonthala@gmail.com>
2024-04-02 19:39:53 +0530

    Adding Login Post Request

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,19 +1,36 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 
-const logSchema = new mongoose.Schema({
- username: String,
- email: String,
- password: String,
-})
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+   
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function(value) {
+        return /\S+@\S+\.\S+/.test(value);
+      },
+      message: props => `${props.value} is not a valid email!`
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  token:{
+    type: Number,
+    required: true,
 
-const userSchema = Joi.object({
-  username: Joi.string().required(), 
-  email: Joi.string().required(),  
-  password: Joi.string().required(),
-
+  }
 });
 
-const UserModel = mongoose.model("user", logSchema);
-module.exports = {UserModel, userSchema};
+const UserModel = mongoose.model("user", userSchema);
 
+module.exports = { UserModel, userSchema };
