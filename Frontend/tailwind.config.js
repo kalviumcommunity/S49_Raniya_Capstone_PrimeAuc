290ce0a0ef902c9c9f58f2fb74d65a021c1855fb
raniyaptla <ra.niyapoonthala@gmail.com>
2024-04-05 12:42:35 +0530

    Task9 : Adding Background

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.js
+const {nextui} = require("@nextui-org/react");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // ...
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        black: "#000000",
+        blue: "#002E62"
+        }
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui(
+    {
+      
+    }
+  )],
+};  
